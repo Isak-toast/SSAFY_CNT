97077c8f57e21f41da286a3f0b2d867cd96d60c8
김이삭 <40036437+Isak-toast@users.noreply.github.com>
2023-03-23 09:54:15 +0900

    MR: conflict contents

--- Data/back/hello/src/main/java/com/example/hello/controller/HelloController.java
+++ Data/back/hello/src/main/java/com/example/hello/controller/HelloController.java
@@ -1,2 +1,14 @@
-package com.example.hello.controller;public class HelloController {
+package com.example.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello, the time at the server is now " + new Date() + "\n";
+    }
 }
